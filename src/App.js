4413361ba296a248d62d68bcbc6b3a768133ef01
hansumin52978 <gkstnals2626@naver.com>
2023-11-02 13:22:11 +0900

    11. 조건부 렌더링 진행, Chart 기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   //지출 항목 객체 배열
-
   const expenses = [
     {
       id: 1,
@@ -28,21 +27,32 @@ const App = () => {
     {
       id: 4,
       title: '엽기떡볶이',
-      price: 14000,
+      price: 17000,
       date: new Date(2021, 3 - 1, 28),
     },
   ];
 
+  //지출 객체배열을 상태변수로 관리
+  const [expenseList, setExpenseList] = useState(expenses);
+
   //ExpenseForm에게 내려보낼 함수
   const addExpenseHandler = (newExpense) => {
     console.log('App 컴포넌트에서 응답함!');
-    console.log(newExpense);
+
+    const modifyExpense = {
+      ...newExpense,
+      id: expenseList[expenseList.length - 1].id + 1,
+    };
+    console.log(modifyExpense);
+
+    setExpenseList([...expenseList, modifyExpense]);
+    console.log(expenseList);
   };
 
   return (
     <>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <Expenses items={expenseList} />
     </>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,9 +3,10 @@ import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
+import ExpenseChart from './ExpenseChart';
 
 const Expenses = ({ items }) => {
-  //선택된 연도 상태값 관리
+  // 선택된 연도 상태값 관리
   const [filteredYear, setFilteredYear] = useState(
     new Date().getFullYear().toString()
   );
@@ -16,9 +17,9 @@ const Expenses = ({ items }) => {
     setFilteredYear(selectedYear);
   };
 
-  // // ExpenseItem을 동적 렌더링하기
+  // ExpenseItem을 동적 렌더링하기
   // const iterateExpenseItem = () => {
-  //   // 자바스크립트 배열의 메소드: map(배열 요소에 적용할 함수)
+  //   // 자바스크립트 배열의 메서드: map(배열 요소에 적용할 함수)
   //   // 콜백 함수의 매개값으로 배열의 요소가 하나씩 전달됨.
   //   // 콜백 함수는 배열 요소의 개수만큼 반복됨.
   //   // map의 리턴값: 함수가 적용된 각 요소가 담긴 새로운 배열이 리턴됨.
@@ -32,20 +33,29 @@ const Expenses = ({ items }) => {
   //   ));
   // };
 
+  const filteredItems = items.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear
+  );
+
+  // 조건부 렌더링을 위한 변수
+  let expenseContent = <p>아직 등록된 지출이 없습니다.</p>;
+
+  if (filteredItems.length > 0) {
+    expenseContent = filteredItems.map((item) => (
+      <ExpenseItem
+        key={item.id}
+        title={item.title}
+        price={item.price}
+        date={item.date}
+      />
+    ));
+  }
+
   return (
     <Card className='expenses'>
       <ExpensesFilter onChangeFilter={filterChangeHandler} />
-
-      {items
-        .filter((item) => item.date.getFullYear().toString() === filteredYear)
-        .map((item) => (
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            price={item.price}
-            date={item.date}
-          />
-        ))}
+      <ExpenseChart />
+      {expenseContent}
     </Card>
   );
 };
