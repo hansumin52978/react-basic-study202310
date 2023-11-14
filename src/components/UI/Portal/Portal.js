05411a6a415b51459c1cbce433eea21bfa40bcf0
hansumin52978 <gkstnals2626@naver.com>
2023-11-14 13:21:14 +0900

    Cart Modal 만들기

--- src/components/UI/Portal/Portal.js
+++ src/components/UI/Portal/Portal.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 const Portal = ({ children: renderComponent, destId }) => {
   return ReactDOM.createPortal(
-    //전달하고자 하는 요소
     renderComponent,
     document.getElementById(destId)
   );
